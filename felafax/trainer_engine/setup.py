5d28a61bc55338ac956b9fcce4c19435372ce439
Felarof <nithin.sonti@gmail.com>
2024-08-25 13:42:54 -0700

    [setup] updated it to point to felafax storage

--- felafax/trainer_engine/setup.py
+++ felafax/trainer_engine/setup.py
@@ -4,14 +4,14 @@ import os
 
 
 def setup_environment():
-    os.environ["HF_HUB_CACHE"] = "/mnt/persistent-disk/hf/"
-    os.environ["HF_HOME"] = "/mnt/persistent-disk/hf/"
+    os.environ["HF_HUB_CACHE"] = "/home/felafax-storage/hf/"
+    os.environ["HF_HOME"] = "/home/felafax-storage/hf/"
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
     # Note: The following shell commands won't work directly in Python.
     # We'll use os.system to execute them.
-    os.system('export HF_HUB_CACHE="/mnt/persistent-disk/hf/"')
-    os.system('export HF_HOME="/mnt/persistent-disk/hf/"')
+    os.system('export HF_HUB_CACHE="/home/felafax-storage/hf/"')
+    os.system('export HF_HOME="/home/felafax-storage/hf/"')
     os.system("export TOKENIZERS_PARALLELISM=false")
 
 
